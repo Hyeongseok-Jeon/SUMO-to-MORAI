0e1c9131deed42a2d60d092e98ab868440f65dcc
Hyeongseok Jeon <521hsgugu@kaist.ac.kr>
2021-10-28 09:47:34 +0900

    sadf

---
+++ sumo2morai/build/npc_ghost_test/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/jhs/Desktop/SUMO-to-MORAI/sumo2morai/src/npc_ghost_test
+# Build directory: /home/jhs/Desktop/SUMO-to-MORAI/sumo2morai/build/npc_ghost_test
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
